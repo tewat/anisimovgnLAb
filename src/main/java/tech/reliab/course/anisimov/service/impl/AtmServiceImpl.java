467d9fcb28ec084c15d7a413d219475553261155
tewat <hollywoodstreets23@gmail.com>
2022-10-01 22:28:31 +0300

    AtmService implemented

--- src/main/java/tech/reliab/course/anisimov/service/impl/AtmServiceImpl.java
+++ src/main/java/tech/reliab/course/anisimov/service/impl/AtmServiceImpl.java
@@ -1,6 +1,40 @@
 package tech.reliab.course.anisimov.service.impl;
 
+import tech.reliab.course.anisimov.entity.BankAtm;
 import tech.reliab.course.anisimov.service.AtmService;
 
 public class AtmServiceImpl implements AtmService {
+    //region ===================== Properties ======================
+    private BankAtm bankAtm = null;
+
+    //region ===================== AtmService implementation ======================
+    @Override
+    public BankAtm getAtm() {
+        return bankAtm;
+    }
+
+    @Override
+    public void setAtm(BankAtm atm) {
+        bankAtm = atm;
+    }
+
+    @Override
+    public Boolean updateAtm(BankAtm atm) {
+        if (bankAtm == atm) {
+            setAtm(atm);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public Boolean deleteAtm(BankAtm atm) {
+        if (bankAtm == atm) {
+            bankAtm = null;
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
