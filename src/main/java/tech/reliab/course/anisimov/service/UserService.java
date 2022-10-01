e1b02f6aa697e10b428f6d261a7fbb8793c93594
tewat <hollywoodstreets23@gmail.com>
2022-10-01 23:06:46 +0300

    USerService implemented

--- src/main/java/tech/reliab/course/anisimov/service/UserService.java
+++ src/main/java/tech/reliab/course/anisimov/service/UserService.java
@@ -1,4 +1,13 @@
 package tech.reliab.course.anisimov.service;
 
+import tech.reliab.course.anisimov.entity.User;
+
 public interface UserService {
+    User getUser();
+
+    void setUser(User user);
+
+    Boolean updateUser(User user);
+
+    Boolean deleteUser(User user);
 }
