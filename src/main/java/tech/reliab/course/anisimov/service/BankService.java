6694ce2be2a3e6664088a8ae11396a6ccb6cefa1
tewat <hollywoodstreets23@gmail.com>
2022-10-01 22:37:56 +0300

    BankService implementation

--- src/main/java/tech/reliab/course/anisimov/service/BankService.java
+++ src/main/java/tech/reliab/course/anisimov/service/BankService.java
@@ -1,4 +1,13 @@
 package tech.reliab.course.anisimov.service;
 
+import tech.reliab.course.anisimov.entity.Bank;
+
 public interface BankService {
+    Bank getBank();
+
+    void setBank(Bank bank);
+
+    Boolean updateBank(Bank bank);
+
+    Boolean deleteBank(Bank bank);
 }
--- src/main/java/tech/reliab/course/anisimov/service/impl/BankServiceImpl.java
+++ src/main/java/tech/reliab/course/anisimov/service/impl/BankServiceImpl.java
@@ -1,6 +1,40 @@
 package tech.reliab.course.anisimov.service.impl;
 
+import tech.reliab.course.anisimov.entity.Bank;
 import tech.reliab.course.anisimov.service.BankService;
 
 public class BankServiceImpl implements BankService {
+    //region ===================== Properties ======================
+    private Bank _bank = null;
+
+    //region ===================== BankService implementation ======================
+    @Override
+    public Bank getBank() {
+        return _bank;
+    }
+
+    @Override
+    public void setBank(Bank bank) {
+        _bank = bank;
+    }
+
+    @Override
+    public Boolean updateBank(Bank bank) {
+        if (_bank == bank) {
+            setBank(bank);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public Boolean deleteBank(Bank bank) {
+        if (_bank == bank) {
+            _bank = null;
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
