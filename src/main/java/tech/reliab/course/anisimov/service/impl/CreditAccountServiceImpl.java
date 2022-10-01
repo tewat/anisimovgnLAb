d6d7d0b0afab7a524068d301b4100277f4fc43d5
tewat <hollywoodstreets23@gmail.com>
2022-10-01 22:43:59 +0300

    CreditAccountService implemented

--- src/main/java/tech/reliab/course/anisimov/service/impl/CreditAccountServiceImpl.java
+++ src/main/java/tech/reliab/course/anisimov/service/impl/CreditAccountServiceImpl.java
@@ -1,6 +1,40 @@
 package tech.reliab.course.anisimov.service.impl;
 
+import tech.reliab.course.anisimov.entity.CreditAccount;
 import tech.reliab.course.anisimov.service.CreditAccountService;
 
 public class CreditAccountServiceImpl implements CreditAccountService {
+    //region ===================== Properties ======================
+    private CreditAccount creditAccount= null;
+
+    //region ===================== CreditAccountService implementation ======================
+    @Override
+    public CreditAccount getAccount() {
+        return creditAccount;
+    }
+
+    @Override
+    public void setAccount(CreditAccount account) {
+        creditAccount = account;
+    }
+
+    @Override
+    public Boolean updateAccount(CreditAccount account) {
+        if (creditAccount == account) {
+            setAccount(creditAccount);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public Boolean deleteAccount(CreditAccount account) {
+        if (creditAccount == account) {
+            creditAccount = null;
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
