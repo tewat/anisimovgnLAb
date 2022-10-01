195e54c4d762fd8235f47603ab6f7591b31e9f83
tewat <hollywoodstreets23@gmail.com>
2022-10-01 23:03:00 +0300

    PaymentAccountService implemented

--- src/main/java/tech/reliab/course/anisimov/service/PaymentAccountService.java
+++ src/main/java/tech/reliab/course/anisimov/service/PaymentAccountService.java
@@ -1,4 +1,13 @@
 package tech.reliab.course.anisimov.service;
 
+import tech.reliab.course.anisimov.entity.PaymentAccount;
+
 public interface PaymentAccountService {
+    PaymentAccount getAccount();
+
+    void setAccount(PaymentAccount account);
+
+    Boolean updateAccount(PaymentAccount account);
+
+    Boolean deleteAccount(PaymentAccount account);
 }
